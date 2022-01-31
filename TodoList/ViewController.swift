55ea6665ddbdbd1b342afe6753f43ef63b2e35a4
Jisoo <dlwltn815@gmail.com>
2022-01-31 12:53:25 +0900

    Add 버튼 action 추가

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    var tasks = [Task]() // Task 배열 생성
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
     }
 
-
+    @IBAction func tapeditButton(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할 일 등록", message: "할 일을 입력해주세요.", preferredStyle: .alert)
+        let registerButton = UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
+            guard let title = alert.textFields?[0].text else { return }
+            let task = Task(title: title, done: false)
+            self?.tasks.append(task)
+            //등록버튼을 눌럿을때 텍스트필드에 있는 값을 가져올 수 있다.
+            // textFields는 배열인데, 우리는 하나만 넣어놧기 때문에 [0]로 접근했음.
+            self?.tableView.reloadData() // add된 할일들을 테이블뷰에 새로새로 업로드해주는 것
+        })
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(cancelButton)
+        alert.addAction(registerButton)
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "할 일을 입력해주세요." })
+        self.present(alert, animated: true, completion: nil)
+    
+    }
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tasks.count
+        }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        // 사용하지 않는 메모리를 낭비하지 않기 위해서 dequeueResusableCell을 이용해서 셀을 재사용 하는 것
+        let task = self.tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        return cell
+        }
+    }
 
